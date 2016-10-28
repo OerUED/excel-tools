9b14a84107f074689568ef1709925fb4d070dd8b
maat <maat@ixiaopu.com>
2016-10-28 14:23:05 +0800

    修改数据格式

--- parsers/yx1111Fushi.js
+++ parsers/yx1111Fushi.js
@@ -3,12 +3,16 @@ var partial_824ListYxBranchProduct = require('./partial_ListYxBranchProduct');
 function yx1111Branch(data) {
   var keys = Object.keys(data);
   var result = {
-    keys: keys
+    keys: []
   };
+
   var thisKey;
-  console.log();
   keys.forEach(key => {
     thisKey = data[key][0].A; // json的key名
+    result.keys.push({
+      key: thisKey,
+      value: key
+    });
     var json = partial_824ListYxBranchProduct(data[key]);
     result[thisKey] = json;
   });
