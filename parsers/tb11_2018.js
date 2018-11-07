d104cf1e86399081510ff247ae8eb467190f8245
nireus <nireus@ixiaopu.com>
2018-11-07 18:39:12 +0800

    2018双十一免单

--- parsers/tb11_2018.js
+++ parsers/tb11_2018.js
@@ -10,22 +10,13 @@ function tb11_2018(data) {
       salesType: item.F,
       prodUrl: item.G,
       remark: item.H,
-      imgUrl: item.I,
-      id: getProdId(item.G, 'id'),
-      itemId: getProdId(item.G, 'item_id')
+      imgUrl: item.I
     }
   });
-  var mdArr = res.filter(i => i.salesType && i.salesType.indexOf('免单') > -1);
-  var msArr = res.filter(i => i.salesType && i.salesType.indexOf('秒杀') > -1);
+  var mdArr = res.filter(i => i.salesType && i.salesType.indexOf('免单') > -1).reverse();
+  var msArr = res.filter(i => i.salesType && i.salesType.indexOf('秒杀') > -1).reverse();
   var result = [mdArr, msArr];
   return result;
-}
-
-function getProdId (url, name) {
-  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-  var r = url.split('?')[1].match(reg);
-  if (r != null) return unescape(r[2]);
-  return null;
 }
 
 module.exports = tb11_2018;
